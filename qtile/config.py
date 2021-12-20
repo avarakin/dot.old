8ce642e45b262a56ade421c41ececd7dd579a91a
Alex Varakin <avarakin@gmail.com>
2021-12-19 20:34:15 -0500

    Mostly working qtile

--- qtile/config.py
+++ qtile/config.py
@@ -42,33 +42,47 @@ keys = [
     Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
+    Key([mod], "Tab", lazy.layout.next(), desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(),
-        desc="Move window down"),
+    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),  desc="Move window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
 
+
+
+    # RESIZE UP, DOWN, LEFT, RIGHT
+    Key([mod, "control"], "Right",
+        lazy.layout.grow_right(),
+        lazy.layout.grow(),
+        lazy.layout.increase_ratio(),
+        lazy.layout.delete(),
+        ),
+    Key([mod, "control"], "Left",
+        lazy.layout.grow_left(),
+        lazy.layout.shrink(),
+        lazy.layout.decrease_ratio(),
+        lazy.layout.add(),
+        ),
+    Key([mod, "control"], "Up",
+        lazy.layout.grow_up(),
+        lazy.layout.grow(),
+        lazy.layout.decrease_nmaster(),
+        ),
+    Key([mod, "control"], "Down",
+        lazy.layout.grow_down(),
+        lazy.layout.shrink(),
+        lazy.layout.increase_nmaster(),
+        ),
+
+
+
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod], 'f', lazy.window.toggle_fullscreen()),
 
-    Key(["mod1"], "space", lazy.spawn("rofi -show drun")),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "Left", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "Right", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "Down", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -76,26 +90,41 @@ keys = [
     # multiple stack panes
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
 
+    # System Commands
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+
+
+
+    #Launchers
+    Key(["mod1"], "space", lazy.spawn("rofi -show drun")),
+    Key([mod], "space", lazy.spawncmd(), desc="Command prompt widget"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "b", lazy.spawn("google-chrome-stable"), desc="Launch Browser"),
+    Key([mod], "c", lazy.spawn("code"),                 desc="Launch Code"),
+    Key([mod], "z", lazy.spawn("zoom"),                 desc="Launch Code"),
+    Key([mod], "p", lazy.spawn("/opt/PixInsight/bin/PixInsight.sh -n"),  desc="Launch PixInsight"),
+    Key([mod], "t", lazy.spawn("teams"), desc="Launch Teams"),    
+#    Key([mod], "f", lazy.spawn("freecad"), desc="Launch FreeCAD"),    
+    Key([mod], "j", lazy.spawn("joplin-desktop"), desc="Launch Joplin"),
+    Key([mod], "v", lazy.spawn("vncviewer"), desc="VNC"),
+    Key([mod], "s", lazy.spawn("systemctl suspend"), desc="Suspend"),
 ]
 
 #groups = [Group(i) for i in "123456789"]
 
 groups = [
-        Group(name='1', matches=None, spawn='google-chrome-stable', layout="max", label='1:main'),
+        Group(name='1', matches=None, spawn='google-chrome-stable', layout="MonadTall", label='1:main'),
         Group(name='2', matches=[Match(wm_class=["PixInsight"])], spawn='/opt/PixInsight/bin/PixInsight.sh -n=3', layout="max", label='2:PI1'),
 #        Group(name='3', matches=None, spawn='/opt/PixInsight/bin/PixInsight.sh -n=4', layout="max", label='3:PI2'),
-        Group(name='3', matches=[Match(wm_class=["code-oss"])], spawn='code', layout="max", label='3:Dev'),
-        
+        Group(name='3', matches=[Match(wm_class=["code-oss"])], spawn='code', layout="MonadTall", label='3:Dev'),
+        Group(name='4', matches=None, spawn='vncviewer', layout="MonadTall", label='4:Astro'),
+
     ]
 
 
@@ -115,25 +144,25 @@ for i in groups:
     ])
 
 layouts = [
-    layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
+    #layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
-    layout.Stack(num_stacks=2),
-    layout.Bsp(),
-    layout.Matrix(),
+    #layout.Stack(num_stacks=2),
+    #layout.Bsp(),
+    #layout.Matrix(),
     layout.MonadTall(),
-    layout.MonadWide(),
-    layout.RatioTile(),
-    layout.Tile(),
-    layout.TreeTab(),
-    layout.VerticalTile(),
-    layout.Zoomy(),
+    #layout.MonadWide(),
+    #layout.RatioTile(),
+    #layout.Tile(),
+    #layout.TreeTab(),
+    #layout.VerticalTile(),
+    #layout.Zoomy(),
 ]
 
 widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
+    font='Roboto Condensed',
+    fontsize=16,
+    padding=0,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -144,11 +173,13 @@ screens = [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
                 widget.Prompt(),
+#                widget.LaunchBar(progs=[('Sleep', '', 'Exit menu')],
+#                             default_icon=home + '/.local/share/icons/logout.png', **wid_defaults),
                 widget.TaskList(),
-                widget.Sep(),
-                widget.WindowName(),
+#                widget.WindowName(),
                 widget.Chord(
                     chords_colors={
+#                widget.Sep(),
                         'launch': ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
@@ -168,7 +199,7 @@ screens = [
                                     distro='Arch',
                                     execute=lazy.spawn("tkpacman"),
                                     foreground=fg_color),
-                widget.Sep(),
+#                widget.Sep(),
 #                widget.СPUGraph(),
                 widget.Sep(),
                 widget.Systray(),
@@ -176,7 +207,7 @@ screens = [
                 widget.Clock(format='%Y-%m-%d %a %I:%M:%S %p'),
                 widget.QuickExit(),
             ],
-            24,
+            28,
         ),
     ),
 ]
