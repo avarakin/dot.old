b2153f441d5f16f9c835d03adcf20ea18a687907
Alex Varakin <avarakin@gmail.com>
2021-12-31 13:09:40 -0500

    Launchers

--- awesome/themes/holo/theme.lua
+++ awesome/themes/holo/theme.lua
@@ -80,7 +80,7 @@ theme.layout_magnifier                          = theme.icon_dir .. "/magnifier.
 theme.layout_floating                           = theme.icon_dir .. "/floating.png"
 theme.tasklist_plain_task_name                  = true
 theme.tasklist_disable_icon                     = true
-theme.useless_gap                               = dpi(4)
+theme.useless_gap                               = dpi(0)
 theme.titlebar_close_button_normal              = theme.default_dir.."/titlebar/close_normal.png"
 theme.titlebar_close_button_focus               = theme.default_dir.."/titlebar/close_focus.png"
 theme.titlebar_minimize_button_normal           = theme.default_dir.."/titlebar/minimize_normal.png"
@@ -418,8 +418,13 @@ function theme.at_screen_connect(s)
             s.mylayoutbox,
             spr_small,
             s.mytag,
+            awful.widget.launcher({ image = os.getenv("HOME") .. "/.config/awesome/icons/google-chrome.png", command = "google-chrome-stable" }),
+            awful.widget.launcher({ image = os.getenv("HOME") .. "/.config/awesome/icons/octopi.png", command = "/usr/bin/octopi" }),
+            awful.widget.launcher({ image = os.getenv("HOME") .. "/.config/awesome/icons/terminator.png", command = "terminator" }),
+            awful.widget.launcher({ image = os.getenv("HOME") .. "/.config/awesome/icons/pixinsight.png", command = "/opt/PixInsight/bin/PixInsight.sh" }),
+            awful.widget.launcher({ image = os.getenv("HOME") .. "/.config/awesome/icons/joplin.png", command = "joplin-desktop" }),
+           
             s.mypromptbox,
-        
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
